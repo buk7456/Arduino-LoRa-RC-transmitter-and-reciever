3f51a08e1b107592645362d9f542d147740ce684
buk7456 <60001726+buk7456@users.noreply.github.com>
2020-08-26 13:34:40 +0300

    Update GFX.h

--- mtx/GFX.h
+++ mtx/GFX.h
@@ -274,37 +274,19 @@ public:
   // This MUST be defined by the subclass:
   virtual void drawPixel(int16_t x, int16_t y, uint16_t color) = 0;
 
-  // TRANSACTION API / CORE DRAW API
-  // These MAY be overridden by the subclass to provide device-specific
-  // optimized code.  Otherwise 'generic' versions are used.
 
-  virtual void writePixel(int16_t x, int16_t y, uint16_t color);
-  virtual void writeFillRect(int16_t x, int16_t y, int16_t w, int16_t h, uint16_t color);
-  virtual void writeFastVLine(int16_t x, int16_t y, int16_t h, uint16_t color);
-  virtual void writeFastHLine(int16_t x, int16_t y, int16_t w, uint16_t color);
-  virtual void writeLine(int16_t x0, int16_t y0, int16_t x1, int16_t y1, uint16_t color);
-
-
-  // BASIC DRAW API
-  // These MAY be overridden by the subclass to provide device-specific
-  // optimized code.  Otherwise 'generic' versions are used.
-  virtual void
-      // It's good to implement those, even if using transaction API
-      drawFastVLine(int16_t x, int16_t y, int16_t h, uint16_t color),
-      drawFastHLine(int16_t x, int16_t y, int16_t w, uint16_t color),
+  void
+      drawVLine(int16_t x, int16_t y, int16_t h, uint16_t color),
+      drawHLine(int16_t x, int16_t y, int16_t w, uint16_t color),
       fillRect(int16_t x, int16_t y, int16_t w, int16_t h, uint16_t color),
-      // Optional and probably not necessary to change
-      drawLine(int16_t x0, int16_t y0, int16_t x1, int16_t y1, uint16_t color),
       drawRect(int16_t x, int16_t y, int16_t w, int16_t h, uint16_t color);
 
   // These exist only with GFX (no subclass overrides)
   void
       drawBitmap(int16_t x, int16_t y, const uint8_t bitmap[], int16_t w, int16_t h, uint16_t color),
-      drawChar(int16_t x, int16_t y, unsigned char c, uint16_t color, uint16_t bg, uint8_t size),
+      drawChar(int16_t x, int16_t y, unsigned char c, uint16_t color),
       setCursor(int16_t x, int16_t y),
       setTextColor(uint16_t c),
-      setTextColor(uint16_t c, uint16_t bg),
-      setTextSize(uint8_t s),
       setTextWrap(boolean w);
       
   virtual size_t write(uint8_t);
@@ -327,10 +309,8 @@ protected:
       _height, // Display w/h as modified by current rotation
       cursor_x, cursor_y;
   uint16_t
-      textcolor,
-      textbgcolor;
+      textcolor;
   uint8_t
-      textsize,
       rotation;
   boolean
       wrap;  // If set, 'wrap' text at right edge of display
