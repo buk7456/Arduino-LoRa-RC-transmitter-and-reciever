343c4f7c5db28b150a77696759f3de52203b6663
buk7456 <60001726+buk7456@users.noreply.github.com>
2020-09-23 16:54:45 +0300

    Update io.h

--- mtx/io.h
+++ mtx/io.h
@@ -1,60 +1,60 @@
 
 //Called in main loop
 void readSwitchesAndButtons(); 
+void determineButtonEvent();
 void readSticks();   
 void computeChannelOutputs(); 
 
 //Helpers
-int deadzoneAndMap(int _theInpt, int _minVal, int _centerVal, int _maxVal, int _deadzn, int _mapMin, int _mapMax);
+
+int deadzoneAndMap(int _input, int _minVal, int _centerVal, int _maxVal, int _deadzn, int _mapMin, int _mapMax);
 int calcRateExpo(int _input, int _rate, int _expo);
 int linearInterpolate(int xValues[], int yValues[], int numValues, int pointX);
 long weightAndOffset(int _input, int _weight, int _diff, int _offset);
 
-// Table for generating stepped sine waveform in mixer
-// SIZE IS 100 BYTES
-// Values have been offset about 100 decimal so value range is 0 to 200 in decimal
-static const unsigned char sineForm[] PROGMEM = {
-  0x64, 0x6A, 0x71, 0x77, 0x7D, 0x83, 0x89, 0x8F, 0x95, 0x9A, 
-  0x9F, 0xA4, 0xA9, 0xAD, 0xB2, 0xB5, 0xB9, 0xBC, 0xBF, 0xC1, 
-  0xC3, 0xC5, 0xC6, 0xC7, 0xC8, 0xC8, 0xC8, 0xC7, 0xC6, 0xC4, 
-  0xC3, 0xC0, 0xBE, 0xBB, 0xB7, 0xB4, 0xB0, 0xAB, 0xA7, 0xA2, 
-  0x9D, 0x97, 0x92, 0x8C, 0x86, 0x80, 0x7A, 0x74, 0x6E, 0x67, 
-  0x61, 0x5A, 0x54, 0x4E, 0x48, 0x42, 0x3C, 0x36, 0x31, 0x2B, 
-  0x26, 0x21, 0x1D, 0x18, 0x14, 0x11, 0x0D, 0x0A, 0x08, 0x05, 
-  0x04, 0x02, 0x01, 0x00, 0x00, 0x00, 0x01, 0x02, 0x03, 0x05, 
-  0x07, 0x09, 0x0C, 0x0F, 0x13, 0x16, 0x1B, 0x1F, 0x24, 0x29, 
-  0x2E, 0x33, 0x39, 0x3F, 0x45, 0x4B, 0x51, 0x57, 0x5E, 0x64
-};
-
 //==================================================================================================
+
 void readSwitchesAndButtons()
 {
-  //Read keymatrix COLUMN 1, ROWS 1 & 2
-  digitalWrite(COL3_MTRX_PIN, LOW);
-  digitalWrite(COL2_MTRX_PIN, LOW);
-  digitalWrite(COL1_MTRX_PIN, HIGH);
-  SwAEngaged = !digitalRead(ROW1_MTRX_PIN); 
-  uint8_t val1 = digitalRead(ROW2_MTRX_PIN);
+  digitalWrite(PIN_COL3, LOW);
+  digitalWrite(PIN_COL2, LOW);
+  digitalWrite(PIN_COL1, HIGH);
+  bool _swa = !digitalRead(PIN_ROW1); 
+  uint8_t _selectKey = digitalRead(PIN_ROW2);
   
-  //Read keymatrix COLUMN 2, ROWS 1 & 2
-  digitalWrite(COL1_MTRX_PIN, LOW);
-  digitalWrite(COL2_MTRX_PIN, HIGH);
-  SwBEngaged = digitalRead(ROW1_MTRX_PIN);
-  uint8_t val2 = digitalRead(ROW2_MTRX_PIN);
+  digitalWrite(PIN_COL1, LOW);
+  digitalWrite(PIN_COL2, HIGH);
+  bool _swb = digitalRead(PIN_ROW1);
+  uint8_t _upKey = digitalRead(PIN_ROW2);
   
-  //Read keymatrix COLUMN 3, ROWS 1 & 2
-  digitalWrite(COL2_MTRX_PIN, LOW);
-  digitalWrite(COL3_MTRX_PIN, HIGH);
-  SwDEngaged = digitalRead(ROW1_MTRX_PIN);
-  uint8_t val3 = digitalRead(ROW2_MTRX_PIN);
-  
-  buttonCode = val1 | val2 << 1 | val3 << 2;
-  
+  digitalWrite(PIN_COL2, LOW);
+  digitalWrite(PIN_COL3, HIGH);
+  bool _swd = digitalRead(PIN_ROW1);
+  uint8_t _downKey = digitalRead(PIN_ROW2);
+
+  //Prevent phantom keys. (These arise if 2 or more buttons are pressed simultanouesly (missing some
+  //diodes). This situation can falsely report switches and lead to loss of control :(
+  uint8_t keySum = _selectKey + _upKey + _downKey;
+  if(keySum > 1) //dont modify anything, just exit
+  {
+    return;
+  }
+
+  //assign
+  SwAEngaged = _swa;
+  SwBEngaged = _swb;
+  SwDEngaged = _swd;
   //SwCState gotten from slave mcu.
   SwCState = (returnedByte >> 6) & 0x03;
+
+  //-- assign buttonCode --
+  if(_selectKey) buttonCode = SELECT_KEY;
+  else if(_upKey) buttonCode = UP_KEY;
+  else if(_downKey) buttonCode = DOWN_KEY;
+  else buttonCode = 0;
   
-  //Play audio when switches are moved
-  uint8_t switchesSum = SwAEngaged + SwBEngaged + SwDEngaged + SwCState;
+  //-- play audio when switches are moved --
+  uint8_t switchesSum = SwAEngaged + SwBEngaged + SwCState + SwDEngaged;
   static uint8_t lastSwitchesSum = switchesSum;
   if(switchesSum != lastSwitchesSum)
     audioToPlay = AUDIO_SWITCHMOVED;
@@ -62,6 +62,68 @@ void readSwitchesAndButtons()
 }
 
 //==================================================================================================
+
+void determineButtonEvent()
+{
+  /* 
+  Modifies the pressedButton, clickedButton and heldButton variables, buttonStartTime 
+  and buttonReleaseTime.
+  Events
+  - pressedButton is triggered once when the button goes down
+  - clickedButton is triggered when the button is released before heldButton event
+  - heldButton is triggered when button is held down long enough  
+  */
+  
+  static uint8_t lastButtonCode = 0;
+  static bool buttonActive = false, longPressActive = false;
+  
+  pressedButton = 0; //clear 
+  
+  if (buttonCode > 0 && (millis() - buttonReleaseTime > 100)) //button down  
+  {
+    if (buttonActive == false)
+    {
+      buttonActive = true;
+      buttonStartTime = millis();
+      lastButtonCode = buttonCode;
+      pressedButton = buttonCode; //event
+    }
+
+    if ((millis() - buttonStartTime > LONGPRESSTIME) && longPressActive == false)
+    {
+      longPressActive = true;
+      heldButton = buttonCode; //event
+    }
+  }
+
+  else //button released
+  {
+    if(buttonActive == true)
+    {
+      buttonReleaseTime = millis();
+      buttonActive = false;
+    }
+    
+    if (longPressActive == true)
+    {
+      longPressActive = false;
+      heldButton = 0;
+      lastButtonCode = 0; //avoids falsely triggering clickedButton event
+    }
+    else
+    {
+      clickedButton = lastButtonCode;
+      lastButtonCode = 0; //enables setting clickedButton event only once
+    }
+  }
+  
+  //play tones
+  if(pressedButton > 0) 
+    audioToPlay = AUDIO_KEYTONE;
+}
+
+//==================================================================================================
+
 void readSticks()
 {
   if (isCalibratingSticks)
@@ -69,23 +131,22 @@ void readSticks()
     return;
   }
 
-  rollIn = analogRead(ROLLINPIN);
-  pitchIn = analogRead(PITCHINPIN);
-  throttleIn = analogRead(THROTTLEINPIN);
-  yawIn = analogRead(YAWINPIN);
-  aux2In = analogRead(AUX2PIN);
+  rollIn  = analogRead(PIN_ROLL);
+  pitchIn = analogRead(PIN_PITCH);
+  throttleIn = analogRead(PIN_THROTTLE);
+  yawIn  = analogRead(PIN_YAW);
+  aux2In = analogRead(PIN_KNOB);
 
-  //add deadzone to roll, pitch, yaw sticks centers. Also add deadband at ends of knob for stability
-  rollIn = deadzoneAndMap(rollIn, rollMin, rollCenterVal, rollMax, deadZonePerc, -500, 500);
-  rollIn = constrain(rollIn, -500, 500);
-  pitchIn = deadzoneAndMap(pitchIn, pitchMin, pitchCenterVal, pitchMax, deadZonePerc, -500, 500);
-  pitchIn = constrain(pitchIn, -500, 500);
-  yawIn = deadzoneAndMap(yawIn, yawMin, yawCenterVal, yawMax, deadZonePerc, -500, 500);
-  yawIn = constrain(yawIn, -500, 500);
+  //add deadzone to roll, pitch, yaw sticks centers. 
+  rollIn  = deadzoneAndMap(rollIn, Sys.rollMin, Sys.rollCenterVal, Sys.rollMax, Sys.deadZonePerc, -500, 500);
+  pitchIn = deadzoneAndMap(pitchIn, Sys.pitchMin, Sys.pitchCenterVal, Sys.pitchMax, Sys.deadZonePerc, -500, 500);
+  yawIn   = deadzoneAndMap(yawIn, Sys.yawMin, Sys.yawCenterVal, Sys.yawMax, Sys.deadZonePerc, -500, 500);
+  
+  //add deadband at extremes of knob for stability
   aux2In = map(aux2In, 20, 1003, -500, 500); 
   aux2In = constrain(aux2In, -500, 500);
 
-  throttleIn = map(throttleIn, thrtlMin, thrtlMax, -500, 500);
+  throttleIn = map(throttleIn, Sys.thrtlMin, Sys.thrtlMax, -500, 500);
   throttleIn = constrain(throttleIn, -500, 500);
  
   //play audio whenever knob crosses center 
@@ -100,13 +161,12 @@ void readSticks()
     audioToPlay = AUDIO_SWITCHMOVED;
     _aux2Region = _CENTER;
   }
-  else if(aux2In > _posSideStart)
-    _aux2Region = _POS_SIDE;
-  else if (aux2In < _negSideStart)
-    _aux2Region = _NEG_SIDE;
+  else if(aux2In > _posSideStart) _aux2Region = _POS_SIDE;
+  else if(aux2In < _negSideStart) _aux2Region = _NEG_SIDE;
 }
 
 //==================================================================================================
+
 void computeChannelOutputs()
 {
   if (isCalibratingSticks)
@@ -115,42 +175,29 @@ void computeChannelOutputs()
   }
 
   ///Declare mix source array, init it to zero, then populate it afterwards 
-  int MixSources[24];   //See the names in UI.
-  for(int i = 0; i < 24; i++) 
+  int MixSources[NUM_MIXSOURCES]; 
+  for(int i = 0; i < NUM_MIXSOURCES; i++) 
     MixSources[i] = 0;
-  
-  enum { 
-    //same order as the name order in UI.
-    Idxsine = 0, 
-    Idxroll, Idxpitch, Idxthrottle, Idxyaw, Idxknob, IdxSwB, IdxSwC, IdxSwD, IdxAil, IdxEle, IdxThrt, IdxRud,
-    IdxNone, 
-    IdxCh1, IdxCh2, IdxCh3, IdxCh4, IdxCh5, IdxCh6, IdxCh7, IdxCh8, IdxVrt1, IdxVrt2 
-  };
-        
-  ///--Mix source - sine waveform
-  //move back and forth btn -500 and 500. Period 2 seconds
-  long _timeInstance = millis() % 2000; 
-  uint8_t _index = map(_timeInstance, 0, 1999, 0, 99) & 0xFF; //map to table indices
-  int _sineVal = pgm_read_byte(&sineForm[_index]); //read from table
-  _sineVal = (_sineVal - 100) * 5; //remove offset and scale to range -500 to 500
-  MixSources[Idxsine] = _sineVal;
-  
+
   ///--Mix source sticks
-  MixSources[Idxroll] = rollIn;
-  MixSources[Idxpitch] = pitchIn;
-  MixSources[Idxthrottle] = throttleIn;
-  MixSources[Idxyaw] = yawIn;
-  MixSources[Idxknob] = aux2In;
+  MixSources[IDX_ROLL] = rollIn;
+  MixSources[IDX_PITCH] = pitchIn;
+  MixSources[IDX_THRTL_RAW] = throttleIn;
+  MixSources[IDX_YAW] = yawIn;
+  MixSources[IDX_KNOB] = aux2In;
   
-  ///--Mix source Switches B, C, and D
-  //These are slowed down to prevent abrupt change
+  ///--Mix source Switches
+  
+  //switches are slowed down to prevent abrupt change
   int _delta = 2 * fixedLoopTime;
+  
   //Switch B
   static int _SwBVal = -500;
   if(SwBEngaged == true) _SwBVal += _delta;
   else _SwBVal -= _delta;
   _SwBVal = constrain(_SwBVal, -500, 500);
-  MixSources[IdxSwB] = _SwBVal;
+  MixSources[IDX_SWB] = _SwBVal;
+  
   //Switch C (3 pos)
   static int _SwCVal = 0;
   int _target;
@@ -169,119 +216,133 @@ void computeChannelOutputs()
     if(_SwCVal > _target)
       _SwCVal = _target;
   }
-  MixSources[IdxSwC] = _SwCVal;
+  MixSources[IDX_SWC] = _SwCVal;
+  
   //Switch D
   static int _SwDVal = -500;
   if(SwDEngaged == true) _SwDVal += _delta;
   else _SwDVal -= _delta;
   _SwDVal = constrain(_SwDVal, -500, 500);
-  MixSources[IdxSwD] = _SwDVal;
+  MixSources[IDX_SWD] = _SwDVal;
   
-  ///--Mix source Ail, Ele, Thr, Rudd
-  //apply rate and expo 
-  if(SwBEngaged == false || DualRateEnabled[AILRTE] == false)
-    MixSources[IdxAil]  = calcRateExpo(rollIn,  RateNormal[AILRTE], ExpoNormal[AILRTE]);
+  ///--Mix source Ail, Ele, Thr, Rud
+  
+  //Ail
+  int _ailIn = rollIn + 5 * (Model.Trim[0] - 100);
+  if(SwBEngaged == false || Model.DualRateEnabled[AILRTE] == false)
+    MixSources[IDX_AIL]  = calcRateExpo(_ailIn,  Model.RateNormal[AILRTE], Model.ExpoNormal[AILRTE]);
   else
-    MixSources[IdxAil]  = calcRateExpo(rollIn,  RateSport[AILRTE], ExpoSport[AILRTE]);
-  if(SwBEngaged == false || DualRateEnabled[ELERTE] == false)
-    MixSources[IdxEle]  = calcRateExpo(pitchIn, RateNormal[ELERTE], ExpoNormal[ELERTE]);
+    MixSources[IDX_AIL]  = calcRateExpo(_ailIn,  Model.RateSport[AILRTE], Model.ExpoSport[AILRTE]);
+  
+  //Ele
+  int _eleIn = pitchIn + 5 * (Model.Trim[1] - 100);
+  if(SwBEngaged == false || Model.DualRateEnabled[ELERTE] == false)
+    MixSources[IDX_ELE]  = calcRateExpo(_eleIn, Model.RateNormal[ELERTE], Model.ExpoNormal[ELERTE]);
   else
-    MixSources[IdxEle]  = calcRateExpo(pitchIn, RateSport[ELERTE], ExpoSport[ELERTE]);
-  if(SwBEngaged == false || DualRateEnabled[RUDRTE] == false)
-    MixSources[IdxRud] = calcRateExpo(yawIn, RateNormal[RUDRTE], ExpoNormal[RUDRTE]);
-  else
-    MixSources[IdxRud] = calcRateExpo(yawIn, RateSport[RUDRTE], ExpoSport[RUDRTE]);
-  //apply throttle curve
+    MixSources[IDX_ELE]  = calcRateExpo(_eleIn, Model.RateSport[ELERTE], Model.ExpoSport[ELERTE]);
+  
+  //Thr
+  int _thrIn = throttleIn + 5 * (Model.Trim[2] - 100);
   int xpoints[5] = {-500, -250, 0, 250, 500};
   int ypoints[5];
   for(int i = 0; i < 5; i++)
-    ypoints[i] = ((int)ThrottlePts[i] - 100) * 5;
-  MixSources[IdxThrt] = linearInterpolate(xpoints, ypoints, 5, throttleIn);
-
-  ///Predefined mixes
-  //So we don't waste the limited mixer slots
-  MixSources[IdxCh1] = MixSources[IdxAil];  //send Ail  to Ch1
-  MixSources[IdxCh2] = MixSources[IdxEle];  //send Ele  to Ch2
-  MixSources[IdxCh3] = MixSources[IdxThrt]; //Send Thrt to Ch3
-  MixSources[IdxCh4] = MixSources[IdxRud];  //Send Rud  to Ch4
+    ypoints[i] = 5 * (Model.ThrottlePts[i] - 100);
+  MixSources[IDX_THRTL_CURV] = linearInterpolate(xpoints, ypoints, 5, _thrIn);
   
-  ///FREE MIXER
+  //Rud
+  int _rudIn = yawIn + 5 * (Model.Trim[3] - 100);
+  if(SwBEngaged == false || Model.DualRateEnabled[RUDRTE] == false)
+    MixSources[IDX_RUD] = calcRateExpo(_rudIn, Model.RateNormal[RUDRTE], Model.ExpoNormal[RUDRTE]);
+  else
+    MixSources[IDX_RUD] = calcRateExpo(_rudIn, Model.RateSport[RUDRTE], Model.ExpoSport[RUDRTE]);
+
+  ///--Predefined mixes
+  //So we don't waste the limited mixer slots
+  MixSources[IDX_CH1] = MixSources[IDX_AIL];  //send Ail  to Ch1
+  MixSources[IDX_CH2] = MixSources[IDX_ELE];  //send Ele  to Ch2
+  MixSources[IDX_CH3] = MixSources[IDX_THRTL_CURV]; //Send Thrt to Ch3
+  MixSources[IDX_CH4] = MixSources[IDX_RUD];  //Send Rud  to Ch4
+  
+  ///--FREE MIXER
   //Takes two input sources, adds or multiplies them, then writes result to specified output
   for(int _mixNum = 0; _mixNum < NUM_MIXSLOTS; _mixNum++)
   {
-    if(MixOut[_mixNum] == IdxNone) //skip to next iteration
+    if(Model.MixOut[_mixNum] == IDX_NONE) //skip to next iteration
       continue;  
     
     //---Input1---
     long _operand1 = 0;
-    if(MixIn1[_mixNum] != IdxNone) //only calculate if input is other than "None"
+    if(Model.MixIn1[_mixNum] != IDX_NONE) //only calculate if input is other than "None"
     {
-      _operand1 = weightAndOffset(MixSources[MixIn1[_mixNum]], MixIn1Weight[_mixNum], 
-                                  MixIn1Diff[_mixNum], MixIn1Offset[_mixNum]);
+      _operand1 = weightAndOffset(MixSources[Model.MixIn1[_mixNum]], Model.MixIn1Weight[_mixNum], 
+                                  Model.MixIn1Diff[_mixNum], Model.MixIn1Offset[_mixNum]);
     }
     //---Input2---
     long _operand2 = 0;
-    if(MixIn2[_mixNum] != IdxNone) //only calculate if input is other than "None"
+    if(Model.MixIn2[_mixNum] != IDX_NONE) //only calculate if input is other than "None"
     {
-      _operand2 = weightAndOffset( MixSources[MixIn2[_mixNum]], MixIn2Weight[_mixNum], 
-                                   MixIn2Diff[_mixNum], MixIn2Offset[_mixNum]);
+      _operand2 = weightAndOffset( MixSources[Model.MixIn2[_mixNum]], Model.MixIn2Weight[_mixNum], 
+                                   Model.MixIn2Diff[_mixNum], Model.MixIn2Offset[_mixNum]);
     }
     //---Mix the two inputs---
     long _output;      
-    if(MixOperator[_mixNum] == 0) //add
+    if(Model.MixOperator[_mixNum] == 0) //add
       _output = _operand1 + _operand2; 
     else //multiply
       _output = (_operand1 * _operand2) / 500; 
     //---Clamp-----
     _output = constrain(_output, -500, 500);
     //---Update sources array for next iteration--- 
-    MixSources[MixOut[_mixNum]] = int(_output);
+    MixSources[Model.MixOut[_mixNum]] = int(_output);
   }
   
   //export mix results for graphing
-  for(int i = 0; i < 8; i++)
-    mixerChOutGraphVals[i] = MixSources[IdxCh1 + i] / 5; //divide by 5 to fit datatype
+  for(int i = 0; i < NUM_PRP_CHANNLES; i++)
+    mixerChOutGraphVals[i] = MixSources[IDX_CH1 + i] / 5; //divide by 5 to fit datatype
   
   ///WRITE TO CHANNELS
-  for(int i = 0; i < 8; i++)
+  for(int i = 0; i < NUM_PRP_CHANNLES; i++)
   {
-    ChOut[i] = MixSources[IdxCh1 + i];
+    ChOut[i] = MixSources[IDX_CH1 + i];
    
-    //---Reverse--
-    if (Reverse[i] == true) 
+    //---Reverse
+    if (Model.Reverse[i] == true) 
       ChOut[i] = 0 - ChOut[i]; 
 
-    //---Subtrim---
-    ChOut[i] += 5 * (Subtrim[i] - 50); 
+    //---Subtrim
+    ChOut[i] += 5 * (Model.Subtrim[i] - 50); 
     
-    //---Check Cut. If specified and switch engaged, overide--
-    if(SwAEngaged == true && CutValue[i] > 0)
-      ChOut[i] = 5 * (CutValue[i] - 101);
+    //---Check Cut. If specified and switch engaged, overide
+    if(SwAEngaged == true && Model.CutValue[i] > 0)
+      ChOut[i] = 5 * (Model.CutValue[i] - 101);
     
-    //---Endpoints--
-    ChOut[i] = constrain(ChOut[i], 5 * (0 - EndpointL[i]), 5 * EndpointR[i]); 
+    //---Endpoints
+    ChOut[i] = constrain(ChOut[i], 5 * (0 - Model.EndpointL[i]), 5 * Model.EndpointR[i]); 
   }
 }
 
 //====================================Helpers=======================================================
 
-int deadzoneAndMap(int _theInpt, int _minVal, int _centerVal, int _maxVal, int _deadzn, int _mapMin, int _mapMax)
+int deadzoneAndMap(int _input, int _minVal, int _centerVal, int _maxVal, int _deadzn, int _mapMin, int _mapMax)
 {
   long _ddZnTmp = (long)(_maxVal - _minVal) * _deadzn;
   _ddZnTmp /= 100;
   int _ddZnVal = int(_ddZnTmp) / 2; //divide by 2 as we apply deadzone about center
   int _mapCenter = (_mapMin / 2) + (_mapMax / 2);
   //add dead zone and map the input
-  int _outpt;
-  if (_theInpt > _centerVal + _ddZnVal)
-    _outpt = map(_theInpt, _centerVal + _ddZnVal, _maxVal, _mapCenter + 1, _mapMax);
-  else if (_theInpt < _centerVal - _ddZnVal)
-    _outpt = map(_theInpt, _minVal, _centerVal - _ddZnVal, _mapMin, _mapCenter - 1);
+  int _output;
+  if (_input > _centerVal + _ddZnVal)
+    _output = map(_input, _centerVal + _ddZnVal, _maxVal, _mapCenter + 1, _mapMax);
+  else if (_input < _centerVal - _ddZnVal)
+    _output = map(_input, _minVal, _centerVal - _ddZnVal, _mapMin, _mapCenter - 1);
   else
-    _outpt = _mapCenter;
-
-  return _outpt;
+    _output = _mapCenter;
+  
+  _output = constrain(_output, _mapMin, _mapMax);
+  return _output;
 }
+
+//--------------------------------------------------------------------------------------------------
 
 int calcRateExpo(int _input, int _rate, int _expo)
 {
@@ -311,10 +372,12 @@ int calcRateExpo(int _input, int _rate, int _expo)
     We also work with only positive x
   */
   
+  _input = constrain(_input, -500, 500); //limit input 
+  
   long x = _input;
   if(_input < 0) 
     x = -x;
-    
+  
   long k = _expo;
   if(_expo > 100)
   {
@@ -341,6 +404,8 @@ int calcRateExpo(int _input, int _rate, int _expo)
     
   return int(y);
 }
+
+//--------------------------------------------------------------------------------------------------
 
 long weightAndOffset(int _input, int _weight, int _diff, int _offset)
 {
@@ -369,12 +434,14 @@ long weightAndOffset(int _input, int _weight, int _diff, int _offset)
   return _outVal;
 }
 
+//--------------------------------------------------------------------------------------------------
+
 int linearInterpolate(int xValues[], int yValues[], int numValues, int pointX)
 {
   //Implementation of linear Interpolation using integers
   //Formula used is y = ((x - x0)*(y1 - y0))/(x1 - x0) + y0;
   long y = 0;
-  if(pointX < xValues[0])
+  if(pointX < xValues[0]) 
     y = yValues[0]; 
   else if(pointX > xValues[numValues - 1])
     y = yValues[numValues - 1];
@@ -396,3 +463,4 @@ int linearInterpolate(int xValues[], int yValues[], int numValues, int pointX)
   }
   return int(y); 
 }
+
