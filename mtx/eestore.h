6b29b1c28ac7e98040db15f400834cf582f88a92
buk7456 <60001726+buk7456@users.noreply.github.com>
2020-11-22 13:35:46 +0300

    Update eestore.h

--- mtx/eestore.h
+++ mtx/eestore.h
@@ -1,15 +1,22 @@
 /* 
   Data arranged in eeprom as follows
-  ------------------------------------------------------------------------------
-  EE_INITFLAG | System Data | 1st Model Data | 2nd Model Data | nth Model Data
-  ------------------------------------------------------------------------------
+  ---------------------------------------------------------------------------------------------
+  FILE_SIGNATURE | EE_INITFLAG | System Data | 1st Model Data | 2nd Model Data | nth Model Data
+  ---------------------------------------------------------------------------------------------
 */
+
+#define EE_FILE_SIGNATURE_ADDR 0  //2 bytes
+#define EE_INITFLAG_ADDR       2  //1 byte
+const uint8_t eeSysDataStartAddress = 3; 
+uint8_t eeModelDataStartAddress; //determined in setup()
+uint8_t numOfModels;             //determined in setup()
 
 void eeReadSysConfig();
 void eeSaveSysConfig();
 void eeReadModelData(uint8_t _mdlNO); 
 void eeSaveModelData(uint8_t _mdlNO);
 void eeCopyModelName(uint8_t _mdlNO, char* _buff);
+
 //helpers
 int getModelDataOffsetAddr(uint8_t _mdlNO);
 
