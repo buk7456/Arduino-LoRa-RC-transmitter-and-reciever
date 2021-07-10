ed2c5025f5aafb2e1e3e1384b32c43265dad7c0a
buk7456 <60001726+buk7456@users.noreply.github.com>
2021-07-10 17:17:02 +0300

    Update common.h

--- mtx/common.h
+++ mtx/common.h
@@ -1,11 +1,149 @@
+#pragma once
 
+void setDefaultSystemParams();
 void setDefaultModelName();
 void setDefaultModelBasicParams();
 void setDefaultModelMixerParams();
 void setDefaultModelMixerParams(uint8_t _mixNo);
 
+//====================== MISC =====================================================================
+
+#define NUM_PRP_CHANNLES 9  //Number of proportional channels ## Leave this
+
+//---- Output channels --------------------
+extern int channelOut[NUM_PRP_CHANNLES];  //Proportional Channels. Centered at 0, range is -500 to 500.
+extern int8_t mixerChOutGraphVals[NUM_PRP_CHANNLES];  //for graphing raw mixer output for channels. range -100 to 100
+
+//---- Sticks -----------------------------
+extern int rollIn, pitchIn, throttleIn, yawIn, knobIn; //Scaled stick values, range -500 to 500
+extern bool isCalibratingSticks;
+extern bool skipThrottleCheck;
+
+//---- Switches ---------------------------
+//2 position
+extern bool swAEngaged;
+extern bool swBEngaged;
+extern bool swDEngaged;
+extern bool swEEngaged;
+extern bool swFEngaged;
+//3 position
+enum {SWUPPERPOS = 0, SWLOWERPOS = 1, SWMIDPOS = 2};
+extern uint8_t swCState; 
+
+//---- Buttons and button events ----------
+
+enum {SELECT_KEY = 1, UP_KEY, DOWN_KEY,};
+
+extern uint8_t buttonCode; 
+
+extern uint32_t buttonStartTime;
+extern uint32_t buttonReleaseTime;
+
+//Button events
+
+#define LONGPRESSTIME 350
+
+extern uint8_t pressedButton; //triggered once when the button goes down
+extern uint8_t clickedButton; //triggered when the button is released before heldButton event
+extern uint8_t heldButton;    //triggered when button is held down long enough
+
+//---- Model timers -----------------------
+extern uint32_t timer1ElapsedTime;
+extern uint32_t timer1LastElapsedTime;
+extern uint32_t timer1LastPaused;
+
+//---- Transmitter Battery -----------------
+extern int battVoltsNow; //millivolts
+
+enum{
+  BATTLOW = 0, 
+  BATTHEALTY
+};
+
+extern uint8_t battState;
+
+//---- Audio ------------------------------
+enum{  
+  AUDIO_NONE = 0, 
+  
+  AUDIO_BATTERYWARN, 
+  AUDIO_THROTTLEWARN, 
+  AUDIO_TIMERELAPSED, 
+  AUDIO_INACTIVITY, 
+  AUDIO_TELEMWARN,
+  AUDIO_BIND_SUCCESS,
+  AUDIO_TRIM_AIL, 
+  AUDIO_TRIM_ELE, 
+  AUDIO_TRIM_THR, 
+  AUDIO_TRIM_RUD,
+  AUDIO_TRIM_MOVED,
+  AUDIO_TRIM_MODE_ENTERED,
+  AUDIO_TRIM_MODE_EXITED,
+  
+  AUDIO_SWITCHMOVED,
+  
+  AUDIO_KEYTONE
+};
+
+extern uint8_t audioToPlay;
+
+//---- Misc -------------------------------
+
+extern bool isRequestingBind;
+extern uint8_t bindStatusCode;  //1 on success, 2 on fail
+
+extern uint32_t inputsLastMoved; //inactivity detection
+
+extern uint8_t transmitterPacketRate;
+extern uint8_t receiverPacketRate;
+
+//---- Telemetry --------------------------
+extern uint16_t telem_volts; // in 10mV, sent by receiver with 12bits.  0x0FFF "No data"
+
+//---- Output channel configuration -----
+extern uint8_t outputChConfig[9]; 
+extern uint8_t maxOutputChConfig[9];
+extern bool gotOutputChConfig;
+extern bool isRequestingOutputChConfig;
+extern bool sendOutputChConfig;
+extern uint8_t receiverConfigStatusCode; //1 on success, 2 on fail
+
+//-----------------------------------------
+extern uint8_t maxNumOfModels;
+
+//---- Main loop control -------------------
+#define fixedLoopTime 27 
+/*in milliseconds. Min 25, Max 30. Should be greater than 
+the time taken by radio module to transmit the entire packet or else the window is missed 
+resulting in much less throughput. It should also be atleast the time taken to draw the UI, else 
+or else the timing becomes inconsistent*/
+
+extern uint32_t thisLoopNum; //main loop counter
+
 
 //====================== SYSTEM PARAMETERS =========================================================
+
+typedef struct  {
+  
+  uint8_t activeModel; 
+  bool rfOutputEnabled;
+  uint8_t rfPower;
+  uint8_t inactivityMinutes;
+  uint8_t soundMode; 
+  uint8_t backlightMode;
+
+  int rollMax, rollMin, rollCenterVal;
+  int yawMax, yawMin, yawCenterVal;
+  int pitchMax, pitchMin, pitchCenterVal;
+  int thrtlMax, thrtlMin;
+  uint8_t deadZonePerc; 
+  
+  bool telemAlarmEnabled;
+  bool telemVoltsOnHomeScreen;
+  
+} sysParams_t;
+
+extern sysParams_t Sys;
 
 enum {
   SOUND_OFF = 0, 
@@ -33,95 +171,64 @@ enum {
   RFPOWER_LAST = RFPOWER_17dBm
 };
 
-/* Structure for system parameters
-*/
-struct sysParams {
-  
-  uint8_t activeModel = 1; //The current model
-  
-  bool rfOutputEnabled = false;
-  uint8_t rfPower = RFPOWER_10dBm;
-  
-  uint8_t inactivityMinutes = 10;
-  
-  uint8_t soundMode = SOUND_ALL; 
-  
-  uint8_t backlightMode = BACKLIGHT_60S;
-
-  int rollMax  = 1023, rollMin  = 0, rollCenterVal = 512;
-  int yawMax   = 1023, yawMin   = 0, yawCenterVal = 512;
-  int pitchMax = 1023, pitchMin = 0, pitchCenterVal = 512;
-  int thrtlMax = 1023, thrtlMin = 0;
-  uint8_t deadZonePerc = 5; //in percentage of stick range
-  
-  //voltage telemetry
-  
-  bool telemAlarmEnabled = true;
-  bool telemVoltsOnHomeScreen = true;
-  
-} Sys;
-
-
 //====================== MODEL PARAMETERS ==========================================================
-
-
-#define NUM_PRP_CHANNLES 9  //Number of proportional channels ## Leave this
 
 #define NUM_MIXSLOTS 12     //More slots results into less models and more ram usage 
 
-/* Structure for model information
-*/
-struct modelParams {
+typedef struct {
   //------- first entity is the modelName ----
   char modelName[9]; //8chars + Null
   
   //------- basic params ---------
   
-  uint16_t Reverse; // each bit represents a channel. 1 is on, 0 is off
-  int8_t EndpointL[NUM_PRP_CHANNLES];   //left endpoint, -100 to 0
-  int8_t EndpointR[NUM_PRP_CHANNLES];   //right endpoint, 0 to 100
-  int8_t Subtrim[NUM_PRP_CHANNLES];     //-20 to 20
-  int8_t Failsafe[NUM_PRP_CHANNLES];    //-101 to 100. -101 means off
+  uint16_t reverse; // each bit represents a channel. 1 is on, 0 is off
+  int8_t endpointL[NUM_PRP_CHANNLES];   //left endpoint, -100 to 0
+  int8_t endpointR[NUM_PRP_CHANNLES];   //right endpoint, 0 to 100
+  int8_t subtrim[NUM_PRP_CHANNLES];     //-20 to 20
+  int8_t failsafe[NUM_PRP_CHANNLES];    //-101 to 100. -101 means off
 
   // Ail, Ele, Rud
-  uint8_t DualRate;  //Bit0 Ail, Bit1 Ele, Bit2 Rud  
-  int8_t RateNormal[3];   //0 to 100
-  int8_t RateSport[3];    //0 to 100
-  int8_t ExpoNormal[3];   //-100 to 100
-  int8_t ExpoSport[3];    //-100 to 100
+  uint8_t dualRate;  //Bit0 Ail, Bit1 Ele, Bit2 Rud  
+  int8_t rateNormal[3];   //0 to 100
+  int8_t rateSport[3];    //0 to 100
+  int8_t expoNormal[3];   //-100 to 100
+  int8_t expoSport[3];    //-100 to 100
 
-  int8_t ThrottlePts[5];  //for interpolation of throttle. Range -100 to 100
+  int8_t throttlePts[5];  //for interpolation of throttle. Range -100 to 100
   
-  int8_t Trim[4];         //for Ail, Ele, Thr, Rud inputs. Values -20 to 20
+  int8_t trim[4];         //for Ail, Ele, Thr, Rud inputs. Values -20 to 20
   
-  uint8_t Slow1Up;   // in tenths of a second
-  uint8_t Slow1Down; // in tenths of a second
-  uint8_t Slow1Src;  // only switches allowed as source
+  uint8_t slow1Up;   // in tenths of a second
+  uint8_t slow1Down; // in tenths of a second
+  uint8_t slow1Src;  // only switches allowed as source
 
-  uint8_t Timer1ControlSrc;
-  uint8_t Timer1Operator;
-  int8_t  Timer1Value;      //-100 to 100
-  uint8_t Timer1InitMins;   //if 0, timer will count up, else count down
+  uint8_t timer1ControlSrc;
+  uint8_t timer1Operator;
+  int8_t  timer1Value;      //-100 to 100
+  uint8_t timer1InitMins;   //if 0, timer will count up, else count down
   
   uint16_t telemVoltsThresh; //as 10mV
 
   //------- mixer params ---------
   
-  uint8_t MixIn1[NUM_MIXSLOTS];  //index in mix sources array
-  int8_t MixIn1Offset[NUM_MIXSLOTS]; //-100 to 100
-  int8_t MixIn1Weight[NUM_MIXSLOTS]; //-100 to 100
-  int8_t MixIn1Diff[NUM_MIXSLOTS];   //-100 to 100
+  uint8_t mixIn1[NUM_MIXSLOTS];  //index in mix sources array
+  int8_t mixIn1Offset[NUM_MIXSLOTS]; //-100 to 100
+  int8_t mixIn1Weight[NUM_MIXSLOTS]; //-100 to 100
+  int8_t mixIn1Diff[NUM_MIXSLOTS];   //-100 to 100
   
-  uint8_t MixIn2[NUM_MIXSLOTS];  //index in mix sources array
-  int8_t MixIn2Offset[NUM_MIXSLOTS]; //-100 to 100
-  int8_t MixIn2Weight[NUM_MIXSLOTS]; //-100 to 100
-  int8_t MixIn2Diff[NUM_MIXSLOTS];   //-100 to 100
+  uint8_t mixIn2[NUM_MIXSLOTS];  //index in mix sources array
+  int8_t mixIn2Offset[NUM_MIXSLOTS]; //-100 to 100
+  int8_t mixIn2Weight[NUM_MIXSLOTS]; //-100 to 100
+  int8_t mixIn2Diff[NUM_MIXSLOTS];   //-100 to 100
   
-  uint8_t MixOper_N_Switch[NUM_MIXSLOTS]; //upper 2 bits for operator, other 6 bits for the switch
+  uint8_t mixOper_N_Switch[NUM_MIXSLOTS]; //upper 2 bits for operator, other 6 bits for the switch
 
-  uint8_t MixOut[NUM_MIXSLOTS];  //index in mix sources array
+  uint8_t mixOut[NUM_MIXSLOTS];  //index in mix sources array
   
-} Model; 
+} modelParams_t;
+
+extern modelParams_t Model; 
+
 
 enum {
   AILRTE = 0, 
@@ -171,196 +278,3 @@ enum { //possible values in MixSwitch array. Max 64 values
   NUM_MIXSWITCHES //should be last
 };
 
-void setDefaultModelName()
-{
-  uint8_t len = sizeof(Model.modelName);
-  for(uint8_t i = 0; i < len - 1; i++) 
-    Model.modelName[i] = ' ';
-  Model.modelName[len - 1] = '\0';
-}
-
-void setDefaultModelBasicParams()
-{
-  //reverse 
-  Model.Reverse = 0;
-  //subtrim, endpoints, failsafe
-  for(uint8_t i = 0; i < NUM_PRP_CHANNLES; i++)
-  {
-    Model.EndpointL[i] = -100;
-    Model.EndpointR[i] = 100;
-    Model.Subtrim[i]   = 0;
-    Model.Failsafe[i]  = -101;
-  }
-  Model.Failsafe[2]  = -100; //specify failsafe on channel 3 (default throttle channel)
-  
-  //trim
-  Model.Trim[0] = 0;
-  Model.Trim[1] = 0;
-  Model.Trim[2] = 0;
-  Model.Trim[3] = 0;
-  
-  //rate and expo, dualrate
-  Model.DualRate = 0;
-  for(uint8_t i = 0; i < 3; i++)
-  {
-    Model.RateNormal[i] = 100;
-    Model.RateSport[i]  = 100;
-    Model.ExpoNormal[i] = 0;
-    Model.ExpoSport[i]  = 0;
-  }
-  
-  //throttle curve
-  Model.ThrottlePts[0] = -100;
-  Model.ThrottlePts[1] = -50;
-  Model.ThrottlePts[2] = 0;
-  Model.ThrottlePts[3] = 50;
-  Model.ThrottlePts[4] = 100;
-  
-  //custom slowed inputs
-  Model.Slow1Src = IDX_SWC; 
-  Model.Slow1Up = 5;
-  Model.Slow1Down = 5;
-  
-  //Timers
-  Model.Timer1ControlSrc = IDX_NONE;
-  Model.Timer1Operator = GREATER_THAN;
-  Model.Timer1Value = 0;
-  Model.Timer1InitMins = 0;
-  
-  //Telemetry
-  Model.telemVoltsThresh = 0;
-}
-
-void setDefaultModelMixerParams(uint8_t _mixNo)
-{
-  Model.MixIn1[_mixNo]        = IDX_NONE; 
-  Model.MixIn1Offset[_mixNo]  = 0;
-  Model.MixIn1Weight[_mixNo]  = 0;
-  Model.MixIn1Diff[_mixNo]    = 0;
-  Model.MixIn2[_mixNo]        = IDX_NONE;
-  Model.MixIn2Offset[_mixNo]  = 0;
-  Model.MixIn2Weight[_mixNo]  = 0;
-  Model.MixIn2Diff[_mixNo]    = 0;
-  Model.MixOper_N_Switch[_mixNo] = MIX_ADD << 6 | SW_NONE;
-  Model.MixOut[_mixNo]        = IDX_NONE;
-}
-
-void setDefaultModelMixerParams()
-{
-  for(uint8_t i = 0; i < NUM_MIXSLOTS; i++)
-    setDefaultModelMixerParams(i);
-}
-
-
-//====================== MISC =====================================================================
-
-
-//---- Output channels --------------------
-int ChOut[NUM_PRP_CHANNLES];  //Proportional Channels. Centered at 0, range is -500 to 500.
-
-int8_t mixerChOutGraphVals[NUM_PRP_CHANNLES];  //for graphing raw mixer output for channels. range -100 to 100
-
-
-//---- Sticks -----------------------------
-int rollIn, pitchIn, throttleIn, yawIn, knobIn; //Scaled stick values, range -500 to 500
-bool isCalibratingSticks = false;
-bool skipThrottleCheck = false;
-
-
-//---- Switches ---------------------------
-//2 position
-bool SwAEngaged = false;
-bool SwBEngaged = false;
-bool SwDEngaged = false;
-bool SwEEngaged = false;
-bool SwFEngaged = false;
-//3 position
-enum {SWUPPERPOS = 0, SWLOWERPOS = 1, SWMIDPOS = 2};
-uint8_t SwCState = SWUPPERPOS; 
-
-
-//---- Buttons and button events ----------
-uint8_t buttonCode = 0; 
-enum {
-  SELECT_KEY = 1, 
-  UP_KEY, 
-  DOWN_KEY,
-};
-
-uint32_t buttonStartTime = 0;
-uint32_t buttonReleaseTime = 0;
-
-//Button events
-#define LONGPRESSTIME 350
-uint8_t pressedButton = 0; //triggered once when the button goes down
-uint8_t clickedButton = 0; //triggered when the button is released before heldButton event
-uint8_t heldButton = 0;    //triggered when button is held down long enough
-
-
-//---- Model timers -----------------------
-uint32_t timer1ElapsedTime = 0;
-uint32_t timer1LastElapsedTime = 0;
-uint32_t timer1LastPaused = 0;
-
-
-//---- Transmitter Battery -----------------
-int battVoltsNow; //millivolts
-enum{BATTLOW = 0, BATTHEALTY};
-uint8_t battState = BATTHEALTY;
-
-
-//---- Audio ------------------------------
-enum{  
-  AUDIO_NONE = 0, 
-  
-  AUDIO_BATTERYWARN, 
-  AUDIO_THROTTLEWARN, 
-  AUDIO_TIMERELAPSED, 
-  AUDIO_INACTIVITY, 
-  AUDIO_TELEMWARN,
-  AUDIO_BIND_SUCCESS,
-  AUDIO_TRIM_AIL, 
-  AUDIO_TRIM_ELE, 
-  AUDIO_TRIM_THR, 
-  AUDIO_TRIM_RUD,
-  AUDIO_TRIM_MOVED,
-  AUDIO_TRIM_MODE_ENTERED,
-  AUDIO_TRIM_MODE_EXITED,
-  
-  AUDIO_SWITCHMOVED,
-  
-  AUDIO_KEYTONE
-};
-uint8_t audioToPlay = AUDIO_NONE;
-
-
-//---- Misc -------------------------------
-
-bool isRequestingBind = false;
-uint8_t bindStatusCode = 0;  //1 on success, 2 on fail
-
-uint32_t inputsLastMoved = 0; //inactivity detection
-
-uint8_t transmitterPacketRate = 0;
-uint8_t receiverPacketRate = 0;
-
-
-//---- Telemetry --------------------------
-uint16_t telem_volts = 0x0FFF; // in 10mV, sent by receiver with 12bits.  0x0FFF "No data"
-
-
-//---- Output channel configuration -----
-uint8_t OutputChConfig[9]; 
-uint8_t maxOutputChConfig[9];
-bool gotOutputChConfig = false;
-bool isRequestingOutputChConfig = false;
-bool sendOutputChConfig = false;
-uint8_t receiverConfigStatusCode = 0; //1 on success, 2 on fail
-
-
-//---- Main loop control -------------------
-const uint8_t fixedLoopTime = 27; /*in milliseconds. Min 25, Max 30. Should be greater than 
-the time taken by radio module to transmit the entire packet or else the window is missed 
-resulting in much less throughput. It should also be atleast the time taken to draw the UI, else 
-or else the timing becomes inconsistent*/
-uint32_t thisLoopNum = 0; //main loop counter
