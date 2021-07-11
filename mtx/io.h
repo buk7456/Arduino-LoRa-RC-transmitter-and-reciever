9b796675b284ebff8f1df104fb9588c19eb275c0
buk7456 <60001726+buk7456@users.noreply.github.com>
2021-07-11 11:52:23 +0300

    Update io.h

--- mtx/io.h
+++ mtx/io.h
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef _IO_H_
+#define _IO_H_
 
 void readSwitchesAndButtons(); 
 void determineButtonEvent();
@@ -8,3 +9,4 @@ void computeChannelOutputs();
 int calcRateExpo(int _input, int _rate, int _expo);
 int linearInterpolate(int xValues[], int yValues[], uint8_t numValues, int pointX);
 
+#endif
